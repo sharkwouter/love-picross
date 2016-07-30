0034bc1e818709599ec3103ceced9eaabfb1acee
Wouter Wijsman <wwijsman@live.nl>
2016-07-30 12:56:24 +0200

    Now checks if the puzzle has been solved and calculates numbers The numbers aren't drawn on the side yet A debug function has been added to show values of squares

--- main.lua
+++ main.lua
@@ -11,6 +11,18 @@ function load_puzzle()
            }
 end
 
+function load_workspace()
+    workspace = {}
+    for y=1,raster_height do
+        workspace [y] = {}
+        for x=1,raster_width do
+            workspace [y] [x] = 0
+        end
+    end
+    
+    return workspace
+end
+
 function love.load()
     game_name = "love-picross"
     
@@ -24,16 +36,10 @@ function love.load()
     block_y = 0
     
     -- Create empty array to draw on
-    workspace = {}
-    for y=1,raster_height do
-        workspace [y] = {}
-        for x=1,raster_width do
-            workspace [y] [x] = 0
-        end
-    end
+    workspace = load_workspace()
     
     -- Calculate values on the side
-    number = calculate_numbers(puzzle)
+    numbers = calculate_numbers(puzzle)
     
     -- Set window properties
     love.window.setMode(block_size*(raster_width+2),block_size*(raster_height+2))
@@ -70,9 +76,72 @@ function draw_workspace(workspace)
     end
 end
 
-function calculate_number(puzzle)
+function calculate_numbers(puzzle)
     --this will be calculating the numbers to be printed on the side
-    return 0
+    hor_numbers = {}
+    for x=1,raster_width do
+        current_number = 0
+        hor_numbers [x] = {}
+        for y=1,raster_height do
+            if puzzle [y] [x] == 1 then
+               current_number = current_number+1
+            elseif current_number > 0 and x > 1 then
+                numbers_amount = table.getn(hor_numbers [x])
+                hor_numbers [numbers_amount+1] = current_number
+            end 
+        end
+    end
+    vert_numbers = {}
+    for y=1,raster_height do
+        current_number = 0
+        vert_numbers [y] = {}
+        for x=1,raster_width do
+            if puzzle [y] [x] == 1 then
+               current_number = current_number+1
+            elseif current_number > 0 and y > 1 then
+                numbers_amount = table.getn(vert_numbers [y])
+                vert_numbers [numbers_amount+1] = current_number
+            end 
+        end
+    end
+    numbers_full = {}
+    numbers_full [1] = hor_numbers
+    numbers_full [2] = ver_numbers
+    return numbers_full
+end
+
+function draw_numbers(numbers)
+--    for 
+end
+
+-- This function is only for 2 dimensional arrays!
+-- It is used to check if the puzzle has been solved yet
+function arrays_equal(array1,array2)
+    equal = 1
+    height = table.getn(array1)
+    width = table.getn(array1[1])
+    for y=1,height do
+        if equal == 0 then
+            break
+        end
+        for x=1,width do
+            if array1 [y] [x] ~= array2 [y] [x] then
+                equal = 0
+            end
+        end
+   end
+   
+   return equal
+end
+
+function draw_debug_info()
+    love.graphics.setColor(255,0,0)
+    for y=1,raster_height do
+        for x=1,raster_width do
+            love.graphics.print(puzzle [y] [x],x*block_size+1,y*block_size)
+            love.graphics.print(workspace [y] [x],x*block_size+1,y*block_size+block_size-14)
+        end
+    end
 end
 
 function love.mousereleased(mouse_x,mouse_y,mouse_button)
@@ -97,9 +166,16 @@ function love.mousereleased(mouse_x,mouse_y,mouse_button)
 end
 
 function love.update(dt)
+    if arrays_equal(workspace,puzzle) == 1 then
+        print("Congratulations! The puzzle has been solved!")
+        --reset the workspace
+        workspace = load_workspace()
+   end
 end
 
 function love.draw()
+    love.graphics.setColor(0,0,0)
     draw_raster(raster_width,raster_height,block_size)
     draw_workspace(workspace)
+--    draw_debug_info()
 end
